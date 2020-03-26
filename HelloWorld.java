ed049478dcddcd39958ae5cea4dcb121aed9ddff
Ubuntu <ubuntu@ip-172-31-46-83.ec2.internal>
2020-03-26 19:12:14 +0000

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// Copyright (c) Ac industries All rights reserved  
+//
+// Created by: Andrew Coxall
+// Created on: Mar 26, 2020
+// This is "hello world" but, they way Dad wants it
+
+public class HelloWorld {
+	public static void main(String[] args) {
+    	System.out.println("Hello World!");
+	}
+}
